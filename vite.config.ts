82cb7b27c1b3c6c731485b016c85a0690e3f394e
Murat Kaan Ilhan <mki19xci@gmail.com>
2026-05-04 14:53:11 +0300

    chore: rename project to tarodan-design and update Vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/tarodan-test/',
+  base: '/tarodan-design/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
